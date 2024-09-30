c3786b226eb588e2cebafdb032c05d58a77f9543
ZIoyEnot <dkarabeckii@gmail.com>
2024-09-30 16:00:20 +0300

    SpecialTechnicsPage
    сделал страницу спец техники и вывел свою функцию на готовом бизнесе, немного поправил там css и код

--- src/components/Page/ReadyBusinessPage/ReadyBusinessPage.js
+++ src/components/Page/ReadyBusinessPage/ReadyBusinessPage.js
@@ -1,156 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "../../../css/ReadyBusinessPage/ReadyBusinessPage.css";
 import Bag from "../../../assets/images/ReadyBusiness/bag-banner.png";
 import Franchise from "../../../assets/images/ReadyBusiness/franchise-banner.png";
 import { Banner } from "./Banner";
 import AdvSlider from "../MainPage/AdvSlider";
 import { Recommendations } from "./Recommendations";
-import IMG from "../../../assets/images/Image.png";
-
-const products = [
-  {
-    image: IMG,
-    description: "Универсальный трактор TFE-895",
-    cost: "7 000 000",
-    position: "г. Краснодар, ул. Пушкина, 59",
-    date: "25 августа 2024",
-    flags: ["Рассрочка", "В кредит"],
-  },
-  {
-    image: IMG,
-    description: "Универсальный трактор TFE-895",
-    cost: "7 000 000",
-    position: "г. Краснодар, ул. Пушкина, 59",
-    date: "25 августа 2024",
-    flags: ["Рассрочка", "В кредит"],
-  },
-  {
-    image: IMG,
-    description: "Универсальный трактор TFE-895",
-    cost: "7 000 000",
-    position: "г. Краснодар, ул. Пушкина, 59",
-    date: "25 августа 2024",
-    flags: ["Рассрочка", "В кредит"],
-  },
-  {
-    image: IMG,
-    description: "Универсальный трактор TFE-895",
-    cost: "7 000 000",
-    position: "г. Краснодар, ул. Пушкина, 59",
-    date: "25 августа 2024",
-    flags: ["Рассрочка", "В кредит"],
-  },
-  {
-    image: IMG,
-    description: "Универсальный трактор TFE-895",
-    cost: "7 000 000",
-    position: "г. Краснодар, ул. Пушкина, 59",
-    date: "25 августа 2024",
-    flags: ["Рассрочка", "В кредит"],
-  },
-  {
-    image: IMG,
-    description: "Универсальный трактор TFE-895",
-    cost: "7 000 000",
-    position: "г. Краснодар, ул. Пушкина, 59",
-    date: "25 августа 2024",
-    flags: ["Рассрочка", "В кредит"],
-  },
-  {
-    image: IMG,
-    description: "Универсальный трактор TFE-895",
-    cost: "7 000 000",
-    position: "г. Краснодар, ул. Пушкина, 59",
-    date: "25 августа 2024",
-    flags: ["Рассрочка", "В кредит"],
-  },
-  {
-    image: IMG,
-    description: "Универсальный трактор TFE-895",
-    cost: "7 000 000",
-    position: "г. Краснодар, ул. Пушкина, 59",
-    date: "25 августа 2024",
-    flags: ["Рассрочка", "В кредит"],
-  },
-  {
-    image: IMG,
-    description: "Универсальный трактор TFE-895",
-    cost: "7 000 000",
-    position: "г. Краснодар, ул. Пушкина, 59",
-    date: "25 августа 2024",
-    flags: ["Рассрочка", "В кредит"],
-  },
-  {
-    image: IMG,
-    description: "Универсальный трактор TFE-895",
-    cost: "7 000 000",
-    position: "г. Краснодар, ул. Пушкина, 59",
-    date: "25 августа 2024",
-    flags: ["Рассрочка", "В кредит"],
-  },
-  {
-    image: IMG,
-    description: "Универсальный трактор TFE-895",
-    cost: "7 000 000",
-    position: "г. Краснодар, ул. Пушкина, 59",
-    date: "25 августа 2024",
-    flags: ["Рассрочка", "В кредит"],
-  },
-  {
-    image: IMG,
-    description: "Универсальный трактор TFE-895",
-    cost: "7 000 000",
-    position: "г. Краснодар, ул. Пушкина, 59",
-    date: "25 августа 2024",
-    flags: ["Рассрочка", "В кредит"],
-  },
-  {
-    image: IMG,
-    description: "Универсальный трактор TFE-895",
-    cost: "7 000 000",
-    position: "г. Краснодар, ул. Пушкина, 59",
-    date: "25 августа 2024",
-    flags: ["Рассрочка", "В кредит"],
-  },
-  {
-    image: IMG,
-    description: "Универсальный трактор TFE-895",
-    cost: "7 000 000",
-    position: "г. Краснодар, ул. Пушкина, 59",
-    date: "25 августа 2024",
-    flags: ["Рассрочка", "В кредит"],
-  },
-];
 
 export default function ReadyBusinessPage() {
-  const [blocks, setBlocks] = useState([
-    <Banner title="Готовый бизнес" image={Bag} />,
-    <Banner title="Франшиза" image={Franchise} franchise />,
-    <AdvSlider />,
-    <Recommendations adv name={"Рекомендации для вас"} products={products} />,
-  ]);
 
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (
-        window.innerHeight + window.scrollY >=
-        document.body.offsetHeight - 600
-      ) {
-        // Если пользователь доскроллил до низа страницы
-        setBlocks((prevBlocks) => [
-          ...prevBlocks,
-          <Recommendations adv={false} products={products} />,
-        ]);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll); // Убираем обработчик при размонтировании
-    };
-  }, []);
-
-  return <div className="readyBusinessPage">{blocks}</div>;
+  return (<>
+      <div className="readyBusinessPage">
+        
+        <Banner title="Готовый бизнес" image={Bag} />
+        <Banner title="Франшиза" image={Franchise} franchise />
+        <AdvSlider />
+        
+      </div>
+      <Recommendations/>
+  </>)
 }
--- src/components/Page/SpecialTechnicsPage/Category.js
+++ src/components/Page/SpecialTechnicsPage/Category.js
@@ -8,10 +8,10 @@ import Combines from "../../../assets/images/SpecialTechnics/Combines.png";
 import Seeders from "../../../assets/images/SpecialTechnics/Seeders.png";
 import Mowers from "../../../assets/images/SpecialTechnics/Mowers.png";
 
-const Block = ({text, img}) => (
-    <div className='Block'>
+const Block = ({ text, img, bgColor }) => (
+    <div className='Block' style={{ backgroundColor: bgColor }}>
         <h3>{text}</h3>
-        <img src={img}/>
+        {img && <img src={img}/>}
     </div>
 )
 
@@ -19,14 +19,14 @@ export default function Category() {
 
     return (
         <div className='Category'>
-            <Block text = "Грузовики" img = {Trucks}/>
-            <Block text = "Трактора" img = {Tractors}/>
-            <Block text = "Экскаваторы" img = {Excavators}/>
-            <Block text = "Погрузчики" img = {Loaders}/>
-            <Block text = "Комбаины" img = {Combines}/>
-            <Block text = "Сеялки" img = {Seeders}/>
-            <Block text = "Косилки" img = {Mowers}/>
-            <Block text = "Все категории →"/> 
+            <Block text="Грузовики" img={Trucks} bgColor="#FFF7D4"/>
+            <Block text="Трактора" img={Tractors} bgColor="#FAFAFA"/>
+            <Block text="Экскаваторы" img={Excavators} bgColor="#DFF9CA"/>
+            <Block text="Погрузчики" img={Loaders} bgColor="#EAEAEA"/>
+            <Block text="Комбаины" img={Combines} bgColor="#DFF9CA"/>
+            <Block text="Сеялки" img={Seeders} bgColor="#FFF7D4"/>
+            <Block text="Косилки" img={Mowers} bgColor="#FAFFF6"/>
+            <Block text="Все категории →" bgColor="#8DF04A"/>
         </div>
     );
 }
